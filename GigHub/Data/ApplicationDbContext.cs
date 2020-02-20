3ea39c7c0e354937cae7904e3c2940e0d3718dbf
“Iliyasu <ilyas.alkali@gmail.com>
2020-02-20 22:38:46 +0100

    Populate Genres Table

--- GigHub/Data/ApplicationDbContext.cs
+++ GigHub/Data/ApplicationDbContext.cs
@@ -13,5 +13,17 @@ namespace GigHub.Data
 
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Genre>().HasData(
+                new Genre{Id = 1, Name = "Blues"},
+                new Genre{Id= 2, Name = "Country"},
+                new Genre{Id= 3, Name = "Jazz"},
+                new Genre{Id = 4, Name = "R & B"},
+                new Genre{Id = 5, Name = "Rock"}
+                );
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ GigHub/Data/Migrations/20200220213718_PopulateGenresTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHub.Data.Migrations
+{
+    public partial class PopulateGenresTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { (byte)1, "Blues" },
+                    { (byte)2, "Country" },
+                    { (byte)3, "Jazz" },
+                    { (byte)4, "R & B" },
+                    { (byte)5, "Rock" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: (byte)1);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: (byte)2);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: (byte)3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: (byte)4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: (byte)5);
+        }
+    }
+}
